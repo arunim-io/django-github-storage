8933e8250c6ef4bc2a922a0887222d7be7a848c9
Mugdha Arunim Ahmed <mugdhaarunimahmed2017@gmail.com>
2022-11-28 22:21:19 +0600

    prepare for deployment

--- github_storage/apps.py
+++ github_storage/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class GithubStorageConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'github_storage'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "github_storage"
